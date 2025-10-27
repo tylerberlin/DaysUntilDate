32e12ff3a9c126943f08b1a3d052fba9819e08d2
tylerberlin <berlity0611@gmail.com>
2025-10-26 23:30:54 -0400

    Set up GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    output: 'export',
+    distDir: 'out',
+    images: {
+        unoptimized: true,
+    },
+    basePath: process.env.NODE_ENV === 'production' ? '/DaysUntilDate' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/DaysUntilDate/' : '',
+}
 
-export default nextConfig;
+export default nextConfig
